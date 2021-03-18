913671653e77edc7234b18b5a2d1b4221fbf5914
ankit9009 <ankitjagtap9009@gmail.com>
2021-03-18 11:45:18 +0530

    first commit

---
+++ src/Compare.java
@@ -0,0 +1,20 @@
+
+public class Compare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry="yes";
+		
+		if(hungry=="yes")
+		{
+			System.out.println("I am hungry");
+		}
+		else
+		{
+			System.out.println("i am not hungry");
+		}
+		
+	}
+
+}
